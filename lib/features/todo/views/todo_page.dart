56e49e4cb2d3fcf195d65f03782478e40cb13431
Shashi Singh <alphasingh8434@gmail.com>
2026-06-19 12:42:05 +0530

    Layman Setup of Hive

--- lib/features/todo/views/todo_page.dart
+++ lib/features/todo/views/todo_page.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final nameController = TextEditingController();
+    final box = Hive.box('user');
+
     return Scaffold(
       appBar: AppBar(title: Text("Todo App")),
-      body: Column(
-        mainAxisAlignment: .center,
-        children: [
-          Text("Here is the list of tasks:-"),
-          const SizedBox(height: 10),
-          Text("asdfasdf"),
-          const SizedBox(height: 10),
-          Text("asdfasdf"),
-          const SizedBox(height: 10),
-          Text("asdfasdf"),
-          const SizedBox(height: 10),
-          Text("asdfasdf"),
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: "Enter Your Name: ",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              TextButton(
+                onPressed: () => {box.put("name", nameController.text)},
+                child: Text("Submit"),
+              ),
+              const SizedBox(height: 10),
+              const Divider(),
+
+              ValueListenableBuilder(
+                valueListenable: box.listenable(),
+                builder: (context, box, child) {
+                  return Text(box.get('name', defaultValue: "User"));
+                },
+              ),
+
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,16 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Hive.initFlutter();
-  await Hive.openBox("user");
-  runApp(const TodoPage());
+  await Hive.openBox('user');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: TodoPage());
+  }
 }
